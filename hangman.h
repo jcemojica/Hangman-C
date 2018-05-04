7ec99339098b4fa6d195949815cb2ecc73da9911
Jyra Coleen Mojica <35748806+jcemojica@users.noreply.github.com>
2018-05-04 12:21:07 +0800

    Play option added

--- hangman.h
+++ hangman.h
@@ -1,11 +1,17 @@
+#define DICTIONARY_SIZE 10
+#define WORD_SIZE 50
+#define WORDS_PER_LEVEL 5
+
 void banner(){
 	system("clear");	//clears the visible surface of the terminal
 	printf("\n");
-	printf("      00   00  000  0000   00  0000000         000   000  000  0000   00\n");
-	printf("     00   00  00 00  00 00  00  00            00 0 0 00  00 00  00 00  00\n");
-	printf("    0000000  0000000  00  00 00  00  000     00  0  00  0000000  00  00 00\n");
-	printf("   00   00  00     00  00   0 00  00    0   00     00  00     00  00   0 00\n");
-	printf("  00   00  00       00  00   0000  0000000 00     00  00       00  00   0000\n");
+	printf("\t##     ##    ###    ##    ##  ######   ##     ##    ###    ##    ##\n");
+	printf("\t##     ##   ## ##   ###   ## ##    ##  ###   ###   ## ##   ###   ##\n");
+	printf("\t##     ##  ##   ##  ####  ## ##        #### ####  ##   ##  ####  ##\n");
+	printf("\t######### ##     ## ## ## ## ##   #### ## ### ## ##     ## ## ## ##\n");
+	printf("\t##     ## ######### ##  #### ##    ##  ##     ## ######### ##  ####\n");
+	printf("\t##     ## ##     ## ##   ### ##    ##  ##     ## ##     ## ##   ###\n");
+	printf("\t##     ## ##     ## ##    ##  ######   ##     ## ##     ## ##    ##\n");
 	printf("\n");
 }
 
@@ -22,6 +28,19 @@ void menu(int *opt){
 	scanf("%i", opt);
 }
 
+void instructions(int *opt){
+	printf("## ##    ##   #######  ######## ########   ##       ##  ######## ######## ##  #######  ##    ##   #######\n");
+	printf("## ###   ##  ##           ##    ##     ##  ##       ## ##           ##    ## ##     ## ###   ##  ##    \n");
+	printf("## ####  ##  ###          ##    ##     ##  ##       ## ##           ##    ## ##     ## ####  ##  ###   \n");
+	printf("## ## ## ##     ###       ##    ## ####    ##       ## ##           ##    ## ##     ## ## ## ##     ###   \n");
+	printf("## ##  ####        ##     ##    ##   ##    ##       ## ##           ##    ## ##     ## ##  ####        ## \n");
+	printf("## ##   ###         ##    ##    ##    ##    ##     ##  ##           ##    ## ##     ## ##   ###         ##\n");
+	printf("## ##    ##   #######     ##    ##     ##    #######    ########    ##    ##  #######  ##    ##   #######\n");
+
+	/*instructions*/
+	/*exit*/
+}
+
 void levels(int *opt){
 	printf("\t\t* * * * * * * * * * *   * * * * * * * * * * *\n");
 	printf("\t\t*                                           *\n");
@@ -35,6 +54,85 @@ void levels(int *opt){
 	scanf("%i", opt);
 }
 
+typedef struct wordtag{
+	char word[50];
+	char category[100];
+}WORD;
+
 int play(int *score, int level){
-	
+	WORD dictionary[10] = {
+							{"Mang Toto", "Kainan sa Elbi"},
+							{"Chicken joy", "Foodz"},
+							{"boot", "125 Terms"},
+							{"fool", "April Fool's Day"},
+							{"Physical Science", "UPLB Building"},
+							{"Sulyaw", "Kainan sa Elbi"},
+							{"Spaghetti", "Foodz"},
+							{"memory management", "125 Terms"},
+							{"mischievous", "April Fool's Day"},
+							{"butterfly bush", "Flowers"}
+               			};
+
+    system("clear");           			
+    switch(level){
+    	case 1: printf("\t\t\t ZUPER EAZY, YOU WON'T ZWEAT\n"); break;
+    	case 2: printf("\t\t\t NOT ZO EAZY\n"); break;
+    	case 3: printf("\t\t\t NOZEBLEEDING\n"); break;
+    	default: break;
+    }
+
+
+    int lives = 7, match = 0, currIndex, i;
+    int visitedIndex[5], v_indicator = 0;
+    char input;
+
+    srand(time(NULL));
+    while(lives != 0){
+    	currIndex = rand() % 10;
+    	while(visited(currIndex, visitedIndex) == 1){
+    		currIndex = rand() % 10;
+    	}
+
+    	visitedIndex[v_indicator] = currIndex;
+    	v_indicator++;
+
+    	WORD currentWord = dictionary[currIndex];
+
+    	printf("\t\t\t\t %s\n", currentWord.category);
+
+    	for(i = 0; i < WORD_SIZE; ++i){
+    		if(isalpha(currentWord.word[i])){
+    			printf("_ ");
+    		}else{
+    			printf("%c", currentWord.word[i]); //in cases of (')
+    		}
+    	}
+    	printf("\n");
+
+    	printf("[0] Exit Game\n");
+    	getchar();
+    	printf("Give me a letter: ");
+    	scanf("%c", &input);
+
+    	switch(input){
+    		case '0': printf("Your score: %i. Goodbye! See you next time!\n", (*score)); break;
+    		default: /*search the letter here*/
+
+    		break;
+    	}
+
+    	break;
+    }
+    
+
+
+    exit(0);
+}
+
+int visited(int index, int array[]){
+	int i;
+	for(i = 0; i < WORDS_PER_LEVEL; ++i){
+		if(index == array[i]) return 1;
+	}
+	return 0;
 }
